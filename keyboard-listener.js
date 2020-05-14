91b451d912e59d3b8dcf7e3763d56b489741b576
Rodrigo <digo_santiago@hotmail.com>
2020-05-13 23:12:46 -0300

    First try tetris in javascript

---
+++ keyboard-listener.js
@@ -0,0 +1,32 @@
+export default function createKeyboardListener(document) {
+
+    const state = {
+        observers:[]
+    }
+
+    function subscribe(observerFunction){
+        state.observers.push(observerFunction)
+    }
+
+    function notifyAll(command){
+        console.log(`Notifying ${state.observers.length} observers`)
+
+        for(const observerFunction of state.observers)
+            observerFunction(command)
+    }
+
+    document.addEventListener('keydown', handleKeydown)
+
+    function handleKeydown(event){
+        const command = event.code;
+        
+        // game.movePlayer(command)
+        notifyAll(command)
+        
+    }
+
+    return {
+        subscribe
+    }
+
+}
